ddda48f38cd9887ae49a2b824c1e76c2acb04e1e
AshikiShinichi <ashiki.shinichi@idh-net.co.jp>
2024-04-05 10:14:55 +0900

    remove unnecessary files

--- shooting/Assets/ButtelManager.cs
+++ shooting/Assets/ButtelManager.cs
@@ -6,11 +6,14 @@ public class ButtelManager : MonoBehaviour
 {
     private Rigidbody rb;
     public float lifetime;
+    [SerializeField] int forceX;
+    [SerializeField] int forceY;
+    [SerializeField] int forceZ;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(0, 0, 20, ForceMode.Impulse); // 一瞬力を加える
+        rb.AddForce(forceX, forceY, forceZ, ForceMode.Impulse); // 一瞬力を加える
         // rb.AddForce(0, 0, 20, ForceMode.Force); // 継続的にを加える
 
         Destroy(gameObject, lifetime);
