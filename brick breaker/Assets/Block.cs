72253c68e5cad1cc8518c641bf94ffe0060f0291
AshikiShinichi <ashiki.shinichi@idh-net.co.jp>
2024-04-05 11:02:19 +0900

    modified pasrt of script(test)

--- brick breaker/Assets/Block.cs
+++ brick breaker/Assets/Block.cs
@@ -5,19 +5,6 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     public GameManager myManager;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(this.gameObject);
